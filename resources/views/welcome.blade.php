96ab3c1a7ec82c1878a4d2bd70682a4cf015e28d
Nguyen Van Vuong <nguyen.van.vuong@framgia.com>
2016-07-31 15:12:46 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Framgia</div>
+                <div class="title">Framgia123</div>
                 <p>Laravel 5</p>
             </div>
         </div>
