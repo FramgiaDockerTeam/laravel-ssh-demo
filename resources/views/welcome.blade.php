e83d5bcfde721ede789ac33298cfa5f88081103f
Nguyen Van Vuong <nguyen.van.vuong@framgia.com>
2016-08-01 16:32:12 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Framgia hihi test</div>
+                <div class="title">Framgia hihi test, muon di tu</div>
                 <p>Laravel 5</p>
             </div>
         </div>
