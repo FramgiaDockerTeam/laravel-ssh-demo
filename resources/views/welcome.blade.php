b2f16bd02e30431e9e1a6e8d624206263b432c6c
Nguyễn Văn Vượng <nguyen.van.vuong@framgia.com>
2016-08-07 12:19:49 +0700

    uuuuu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,7 @@
         </style>
     </head>
     <body>
+        <p>AAAAAAAAAAA</p>
         <div class="container">
             <div class="content">
                 <div class="title">Framgia Love Cui Bap</div>
