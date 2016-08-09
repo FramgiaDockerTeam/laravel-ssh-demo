074b9966bd67f5222254bb2acb00179238b367e0
Nguyen Van Vuong <nguyen.van.vuong@framgia.com>
2016-08-09 09:59:39 +0700

    CD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Framgia Love Cui Bap</div>
+                <div class="title">Framgia Love</div>
                 <p>Laravel 5</p>
             </div>
         </div>
